37bf397e74f83d77c4f364dc480cd3ed74d5d282
Luiz Paulo Franz <luizpaulofranz@gmail.com>
2019-01-24 12:48:15 -0200

    adds User model, schema and resolver files, type Query definition.

---
+++ schema.js
@@ -0,0 +1,26 @@
+// the objects in models and the ones described here, must expose the same object
+exports.typeDefs = `
+    
+    type Recipe {
+        name: String!
+        category: String!
+        description: String!
+        instructions: String!
+        createdDate: String
+        likes: Int
+        username: String
+    }
+
+    type User {
+        username: String! @unique
+        password: String!
+        email: String!
+        joinDate: String
+        favorites: [Recipe]
+    }
+
+    type Query {
+        getAllRecipes: [Recipe]
+    }
+
+`;
--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 // to read our env variables file
-require("dotenv").config({path: "variables.env"});
+require('dotenv').config({path: 'variables.env'});
+
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+// graphql express middlewares
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
 
 // connects to database
 mongoose.connect(process.env.MONGO_URI)
@@ -10,6 +26,18 @@ mongoose.connect(process.env.MONGO_URI)
 
 // initialize application
 const app = express();
+// set graphiql tool viwer, wich uses the endpoint seted bellow
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+// connect schenas with graphQl
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+        schema: schema,
+        context: {
+            Recipe,
+            User
+        }
+    }
+));
+
 const PORT = process.env.PORT || 4444;
 app.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}.`);

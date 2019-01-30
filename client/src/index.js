65110db5bae6861cc575657bd6b81d282a402a04
Luiz Paulo Franz <luizpaulofranz@gmail.com>
2019-01-30 12:52:40 -0200

    Adds apollo on react app.

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// react - apollo
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4444/graphql'
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, 
+    document.getElementById('root')
+);
 

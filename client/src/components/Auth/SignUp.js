e2585d2765779790e3f8cb27166c32066bb7231e
Luiz Paulo Franz <luizpaulofranz@gmail.com>
2019-02-04 13:14:36 -0200

    Adds State management on Client Sign Up.

--- client/src/components/Auth/SignUp.js
+++ client/src/components/Auth/SignUp.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
 class SignUp extends React.Component {
+
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: ""
+    };
+
+    handleChange = event => {
+        // extract name and value from target object
+        const { name, value } = event.target;
+        this.setState({[name]: value });
+    };
+
     render() {
         return (
             <div className="App">
                 <h2 className="App">Sign Up</h2>
                 <form className="form">
-                    <input type="text" name="username" placeholder="User Name" />
-                    <input type="email" name="email" placeholder="email@address.com" />
-                    <input type="password" name="password" placeholder="Password" />
-                    <input type="password" name="passwordConfirmation" placeholder="Password Confirmation" />
+                    <input type="text" name="username" placeholder="User Name" onChange={this.handleChange} value={this.state.username} />
+                    <input type="email" name="email" placeholder="email@address.com" onChange={this.handleChange} value={this.state.email} />
+                    <input type="password" name="password" placeholder="Password" onChange={this.handleChange} value={this.state.password} />
+                    <input type="password" name="passwordConfirmation" placeholder="Password Confirmation" onChange={this.handleChange} value={this.state.passwordConfirmation} />
                     <button type="submit" className="button-primary">Submit</button>
                 </form>
             </div>

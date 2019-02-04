e580b096eb940f589d4dd8ef6fa6c0f2ffa11b6e
Luiz Paulo Franz <luizpaulofranz@gmail.com>
2019-02-04 12:58:38 -0200

    Adds react-router-dom in our client, manage the routes to new created components.

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import './index.css';
+
 import App from './components/App';
+import SignIn from './components/Auth/SignIn';
+import SignUp from './components/Auth/SignUp';
 
 // react - apollo
 import ApolloClient from 'apollo-boost'
@@ -11,9 +15,20 @@ const client = new ApolloClient({
     uri: 'http://localhost:4444/graphql'
 });
 
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route path="/" exact component={App} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+            <Redirect to="/" />
+        </Switch>
+    </Router>
+)
+
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <App />
+        <Root />
     </ApolloProvider>, 
     document.getElementById('root')
 );

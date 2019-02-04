c6dae0a14372a8bfc3cf7f17e71424b57b677242
Luiz Paulo Franz <luizpaulofranz@gmail.com>
2019-02-04 13:04:36 -0200

    Adds client sign up form.

--- client/src/components/Auth/SignUp.js
+++ client/src/components/Auth/SignUp.js
@@ -3,8 +3,15 @@ import React from 'react';
 class SignUp extends React.Component {
     render() {
         return (
-            <div>
-                SignUp
+            <div className="App">
+                <h2 className="App">Sign Up</h2>
+                <form className="form">
+                    <input type="text" name="username" placeholder="User Name" />
+                    <input type="email" name="email" placeholder="email@address.com" />
+                    <input type="password" name="password" placeholder="Password" />
+                    <input type="password" name="passwordConfirmation" placeholder="Password Confirmation" />
+                    <button type="submit" className="button-primary">Submit</button>
+                </form>
             </div>
         )
     }

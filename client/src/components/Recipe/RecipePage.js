0e772965fcb418644eedb1068345f63c3444c8af
Luiz Paulo Franz <luizpaulofranz@gmail.com>
2019-02-22 13:07:59 -0300

    Adds client side implementation to consume getRecipe(_id) query.

--- client/src/components/Recipe/RecipePage.js
+++ client/src/components/Recipe/RecipePage.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
+import { Query } from 'react-apollo';
+import { GET_RECIPE } from '../../queries/index';
+
 // the match object comes from withRouter, and allow us to get the URL params
 const RecipePage = ({ match }) => {
     const { _id } = match.params; // to get the URL params
     console.log(_id);
     return (
-        <div>
-            ReecipePage
-        </div>
+        <Query query={GET_RECIPE} variables={{_id}}>
+            { (data, loading, error) => {
+                if (loading) return <div>Loading...</div>
+                if (error) return <div>Error...</div>
+                console.log(data);
+                return (
+                    <div>
+                        ReecipePage
+                    </div>
+                );
+            }}
+        </Query>
     )
 }
     

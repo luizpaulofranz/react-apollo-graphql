d0a7a91539ab7df2a10c81cb37032a609c16f78f
Luiz Paulo Franz <luizpaulofranz@gmail.com>
2019-03-11 13:02:01 -0300

    Handle toggle like/unlike button UI.

--- client/src/components/Recipe/LikeRecipe.js
+++ client/src/components/Recipe/LikeRecipe.js
@@ -7,32 +7,57 @@ import { LIKE_RECIPE } from '../../queries/index';
 class LikeRecipe extends React.Component {
 
     state = {
+        liked: false,
         username: '',
         email: ''
     }
 
     componentDidMount() {
         if (this.props.session.getCurrentUser) {
-            const { username, email } = this.props.session.getCurrentUser;
-            console.log(email);
-            this.setState({ username, email });
+            
+            const { username, email, favorites } = this.props.session.getCurrentUser;
+            const { _id } = this.props;
+            // returns true if this index exists
+            const prevLike = (favorites.findIndex(favorite => favorite._id === _id) > -1);
+            
+            this.setState({ 
+                liked: prevLike,
+                username, 
+                email 
+            });
         }
     }
 
-    handleLike = (likeRecipe) => {
-        likeRecipe().then( ({data}) => {
+    // to handle like/unlike
+    handleClick = (likeRecipe) => {
+        this.setState(prevState => ({
+            liked: !prevState.liked
+        }),
+            () => this.handleLike(likeRecipe)
+        )
+    }
 
-        })
+    handleLike = (likeRecipe) => {
+        if (this.state.liked) {
+            likeRecipe().then( async ({data}) => {
+                await this.props.refetch();
+            });
+        } else {
+            console.log('Unlike');
+        }
     }
 
     render() {
-        const { username, email } = this.state;
+        const { username, email, liked } = this.state;
         const { _id } = this.props;
 
         return (
             <Mutation mutation={LIKE_RECIPE} variables={{_id, email}}>
-            { likeRecipe => (
-                username && <button onClick={() => this.handleLike(likeRecipe)}>Like</button>
+            { likeRecipe => 
+                username && (
+                <button onClick={() => this.handleClick(likeRecipe)}>
+                    {!liked ? 'Like' : 'Unlike'}
+                </button>
             )}
             </Mutation>
         );
